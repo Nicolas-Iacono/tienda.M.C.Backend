4659733783ac2281d9bb29f7cdedf4d4707cb5f3
Nicolas Iacono <nicoiacono96@gmail.com>
2025-02-18 16:25:02 -0300

    corrigiendo variables pra frontend

--- config/axiosConfig.js
+++ config/axiosConfig.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
+const dotenv = require("dotenv");
+dotenv.config({
+    path: process.env.NODE_ENV === "production" ? ".env.production" : ".env.development",
+  });
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000', // Cambia al puerto de tu backend
+    baseURL: process.env.API_URL || 'http://localhost:5000', // Cambia al puerto de tu backend
     headers: {
         'Content-Type': 'application/json',
     },
