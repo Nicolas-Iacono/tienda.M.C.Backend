e8c526d45f57ba1883c36ff3310983e7213609e8
Nicolas Iacono <nicoiacono96@gmail.com>
2025-02-25 12:17:30 -0300

    commit de despliegue, agregando cors a movimientoconsiente.com

--- app.js
+++ app.js
@@ -20,7 +20,12 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://movimientoconsiente.com', 'https://www.movimientoconsiente.com'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(passport.initialize());
 
